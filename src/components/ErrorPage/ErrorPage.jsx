6253c411bbee06341504b842b1bac30f90c709ad
bossnoyon00 <bossnoyon584@gmail.com>
2023-04-12 17:12:21 +0600

    some changes document

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import { useNavigation } from 'react-router-dom';
+import { Link, useNavigation } from 'react-router-dom';
 import Spinner from '../Spinner/Spinner';
 
 const ErrorPage = () => {
-    const navigation = useNavigation();
     return (
         <div className='px-4 text-center py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
-            <div>
-                {navigation.state === 'loading' && <Spinner></Spinner>}
-            </div>
-            <div className='mt-40'>
+            <div className='mt-40 mb-8'>
                 <h1 className='text-4xl text-red-900'>
                     <b>404</b></h1>
                 <h1 className='text-5xl text-orange-700'>This Site is not Available!!!
                 </h1>
             </div>
+            <Link to='/'><button style={{ backgroundColor: 'rgba(126, 144, 254, 1)' }} className='btn btn-primary'>HOME PAGE</button></Link>
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import Header from '../Header/Header';
 import { Outlet, useNavigation } from 'react-router-dom';
 import Spinner from '../Spinner/Spinner';
+import ErrorPage from '../ErrorPage/ErrorPage';
 
 const Home = () => {
     const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
-
-            <div>
-                {navigation.state === 'loading' && <Spinner></Spinner>}
-            </div>
             <Outlet></Outlet>
         </div>
     );
